29d0d8b63cce81b754f5b75fd351cf8ab9b502d7
PaulRobinson07 <robinsonpaul1207@gmail.com>
2025-08-25 18:30:01 -0600

    Made the schedule work for more than one event and functionality to many links

--- code/event.js
+++ code/event.js
@@ -1,6 +1,19 @@
-balls = document.getElementsByClassName("event");
-dick = document.getElementsByClassName("schedule_link");
-function toggle() {
-	console.log("success");
-	balls[0].classList.toggle("hidden");
+events = document.getElementsByClassName("event");
+exits = document.getElementsByClassName("exit_button");
+buttons = document.getElementsByClassName("schedule_button");
+function toggle(a) {
+	events[a].classList.toggle("hidden");
 }
+function initialize_toggles() {
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', function() {
+			events[i].classList.toggle("hidden");
+		});
+		exits[i].addEventListener('click', function() {
+			events[i].classList.toggle("hidden");
+		});
+		console.log(i);
+	}
+}
+
+initialize_toggles();
